75a634002a07c2c3c86570717dd3830e05a7e8de
khaleel siaj <191004@ppu.edu.ps>
2021-10-01 00:26:54 +0300

    task completed

--- python-pass.py
+++ python-pass.py
@@ -1,7 +1,23 @@
+#first we create an empty string to store the value in
+#then we loop backwords through out string
+#then update the variable if matched
 
 
-class Solution:
+def LongestPal(s) -> str:
+    palindrome = ''
 
-    @staticmethod
-    def longest_palindromic(s: str) -> str:
-        pass
+    for i in range(len(s)):
+
+        for j in range(len(s), i, -1):
+
+            if len(palindrome) >= j-i:
+                break
+
+            elif s[i:j] == s[i:j][::-1]:
+                palindrome = s[i:j]
+                break
+
+    print(palindrome) 
+    
+    
+LongestPal("babad") 
